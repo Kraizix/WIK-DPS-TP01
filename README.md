90cde5fceab86c4f9cfa496c68c4b1095a4cff50
KGRANGER <kevin.granger@ynov.com>
2022-10-23 20:05:21 +0200

    web server in rust + README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # WIK-DPS-TP01
+
+Serveur web développé en rust en utilisant le moins de dépendances possible.
+Le serveur est une API qui renvoit les headers d'une requête sur la route /ping (HTTP GET)).
+Toutes les autres routes renvoient vers une erreur 404
+Le port d'écoute du serveur est modifiable via le fichier .env (variable PING_LISTEN_PORT).
+
+## Lancer le projet :
+
+Dans un premier temps il faut télécharger [rust](https://www.rust-lang.org/tools/install) ainsi que cargo.
+
+Ensuite il faut cloner le git contenant le projet : `git clone https://github.com/Kraizix/WIK-DPS-TP01.git`
+
+Il ne reste plus qu'à build et run le projet via l'invite de commandes dans le dossier du projet:
+
+- `cd WIK-DPS-TP01`
+- `cargo build`
+- `cargo run`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    env,
+    thread,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+use dotenv::dotenv;
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let host;
+    match env::var("PING_LISTEN_PORT"){
+        Ok(val) => host = format!("127.0.0.1:{val}"),
+        Err(_e) => host = "127.0.0.1:7878".to_string(),
+    };
+    println!("Listening on: {}", host);
+    let listener = TcpListener::bind(host).unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request: Vec<_> = buf_reader.lines().map(|result | result.unwrap()).take_while(|line| !line.is_empty()).collect();
+    let request_line = &request[0];
+    if request_line == "GET /ping HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let mut res: Vec<String> = Vec::new();
+        for data in request[1..].into_iter() {
+            let (s1, s2) = data.split_once(": ").unwrap();
+            res.push(format!("\"{}\": \"{}\"",s1,s2));
+        }
+        let response = format!("{status_line}\r\nContent-Type: application/json\r\n\r\n{{{}}}",res.join(","));
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        stream.write_all(status_line.as_bytes()).unwrap();
+    }
 }
